4f0a57144e57c43d6700342bcab4a29cdaa5687e
Kumar Anuj <kumar.anuj@magicsw.com>
2022-12-10 23:40:41 +0530

    Added console for prod

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,6 @@
 export const returnUrl = (context) => {
     if (process.env.NODE_ENV === "production") {
+        console.log('context',context)
         return `https://${context.req.rawHeaders[1]}`;
       } else if (process.env.NODE_ENV !== "production") {
         return "http://localhost:3000";
