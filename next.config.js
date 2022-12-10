f85e6c6976d2eccee34353db07a1d36b0250b08c
ANUJ KUMAR <41914658+anuj26298@users.noreply.github.com>
2022-12-10 22:45:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com']
   },
-}
+  env: {
+    MONGO_URI:"mongodb+srv://cyberaura:Cyb3rAura559%23@cluster0.owfyg69.mongodb.net/?retryWrites=true&w=majority&ssl=true"
+  }
 
 module.exports = nextConfig
