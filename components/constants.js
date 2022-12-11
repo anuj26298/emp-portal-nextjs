27d7717bd4438d2752cd012e380b16f162ded58f
Kumar Anuj <kumar.anuj@magicsw.com>
2022-12-11 21:02:00 +0530

    Changed username page to create

--- components/constants.js
+++ components/constants.js
@@ -1,3 +1,3 @@
-export const pages = [{ id: 'home', title: 'Home', link: '/' }, { id: 'about', title: 'About', link: '/about' }, { id: 'viewAllEmp', title: 'Employees List', link: '/viewallEmp' },{ id: 'user', title: 'Create New User', link: '/userName' }]
+export const pages = [{ id: 'home', title: 'Home', link: '/' }, { id: 'about', title: 'About', link: '/about' }, { id: 'viewAllEmp', title: 'Employees List', link: '/viewallEmp' },{ id: 'user', title: 'Create New User', link: '/create' }]
 
 export const designations = ['Software Engineer','IT Engineer','Software Engineer','Accounts Manager','Scrum Master','Manager','Team Lead']
